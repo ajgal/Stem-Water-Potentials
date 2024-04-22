573d9bacbe5efc932c9f1050faab34f9da593040
Andrew Gal <112126844+ajgal@users.noreply.github.com>
2024-04-22 15:53:44 -0700

    Add files via upload

--- Weekly_SWP_Plot.R
+++ Weekly_SWP_Plot.R
@@ -2,14 +2,17 @@
 
 setwd("/Users/ajgal/Documents/WES_SWP_Data") # set the working directory
 
-install.packages("ggplot2")   # install packages (only need to run this the first time you use this code)
-install.packages("readxl")
+#install.packages("ggplot2")   # install packages (only need to run this the first time you use this code; remove the "#" to run it)
+#install.packages("readxl")
 
 library(ggplot2)  # load libraries
 library(readxl)
 
 swp <- read_xlsx("SWP_04192024.xlsx") # read excel file with data; change file name to choose sampling date.
 swp <- as.data.frame(swp) # convert excel file to a data frame
+
+baseline_DRI <- -6.3 # change these values based on the air temp and relative humidity conditions at the time of sampling
+baseline_drip <- -6.1
 
 swp_plot <- ggplot(swp, aes(x = block, y = reading_bar, color = block)) +
               geom_jitter(show.legend = FALSE, width = 0.1, size = 3) + #jitterplot in ggplot2
@@ -26,9 +29,9 @@ swp_plot <- ggplot(swp, aes(x = block, y = reading_bar, color = block)) +
                                 name = "Treatment") +
               scale_y_continuous(limits = c(-15, 0), breaks = seq(0, -15, by = -1)) + # set the y axis limits
               scale_x_discrete(position = "top") + # reverse y axis order
-              geom_segment(aes(x = 0.6, xend = 1.4, y = -6.3, yend = -6.3), 
+              geom_segment(aes(x = 0.6, xend = 1.4, y = baseline_DRI, yend = baseline_DRI), 
                            linetype = "dashed", linewidth = 1.25, color = "black") + # DRI baseline SWP horizontal bar
-              geom_segment(aes(x = 1.6, xend = 2.4, y = -6.1, yend = -6.1), 
+              geom_segment(aes(x = 1.6, xend = 2.4, y = baseline_drip, yend = baseline_drip), 
                            linetype = "dashed", linewidth = 1.25, color = "black") # drip baseline SWP horizontal bar
 
 swp_plot # plot figure
